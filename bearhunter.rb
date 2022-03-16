3141b91ce96fde8d17d1a0dd5baae5c348f507e0
Artem Shvetsov <artem.shvetsov@raiffeisen.ua>
2022-03-16 20:05:08 +0200

    feat: dependeces

--- bearhunter.rb
+++ bearhunter.rb
@@ -3,16 +3,17 @@ class Bearhunter < Formula
   homepage "https://github.com/aashvetsov/bearhunter"
   url "https://github.com/aashvetsov/bearhunter.git", tag: "0.1.0"
 
-  depends_on xcode: ["13.2.1", :build]
+  depends_on :xcode => "13.2.1"
+  depends_on "cocoapods"
 
   def install
     system "make", "build", "prefix=#{prefix}"
-    bin.install ".build/universal/bearhunter" => "bearhunter"
+    bin.install ".build/universal/bearhuntercli" => "bearhuntercli"
     system "make", "cleanup", "prefix=#{prefix}"
   end
 
   test do
-    system "#{bin}/bearhunter", "--path ./"
+    system "#{bin}/bearhuntercli", "--path ./"
   end
 
 end
